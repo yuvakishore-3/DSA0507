c4123e2cdefd12f1f3cb7b0b93bbe9c1296afdbe
Yuva kishore <142191797+yuvakishore-3@users.noreply.github.com>
2024-03-18 08:14:55 +0530

    Delete 7 qpds.py

--- 7 qpds.py
+++
@@ -1,9 +0,0 @@
-import pandas as pd
-sales_data = {
-    'Item': ['A', 'B', 'A', 'C', 'B', 'C'],
-    'Units_Sold': [10, 20, 15, 30, 25, 20]
-}
-df = pd.DataFrame(sales_data)
-pivot_table = pd.pivot_table(df, values='Units_Sold', index='Item', aggfunc='sum')
-print("Pivot Table - Item wise unit sold:")
-print(pivot_table)
